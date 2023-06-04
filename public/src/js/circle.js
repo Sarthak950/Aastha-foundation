7c955a8229d1c42d965713b8a5548510f9135329
Sarthak950 <panditsamsar950@gmail.com>
2023-06-04 17:12:54 +0530

    fixed the approach to fake the div on scroll and added some fake divs that to be removed further might implement gsap for ease

--- public/src/js/circle.js
+++ public/src/js/circle.js
@@ -1,7 +1,14 @@
-window.addEventListener("mousemove", (e)=>{
 
-    document.getElementById("cursor").style.left = e.pageX + "px";
-    document.getElementById("cursor").style.top = e.pageY + "px";
+
+
+window.addEventListener("mousemove", (e)=>{
+    // document.getElementById("cursor").style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
+    document.getElementById("cursor").style.left = `${e.clientX}px`;
+    document.getElementById("cursor").style.top = `${e.clientY}px`;
+    //transform the div to the 50%
+        document.getElementById("cursor").style.transform = "translate(-50%, -50%)" 
+
 })
+
 
 
